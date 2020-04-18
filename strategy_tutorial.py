dcb328f15d9797ba31e811aed6deca4de20ae859
Mavis Zeng <maviszeng@MacBook560B7920.hsd1.ca.comcast.net>
2020-04-17 17:29:52 -0700

    Fixed merge function evaluations for any stock

--- strategy_tutorial.py
+++ strategy_tutorial.py
@@ -16,44 +16,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
-def initialize(ticker):
-    data = pdr.get_data_yahoo(ticker,start=datetime.datetime(2014, 10, 1), 
-                          end=datetime.datetime(2020, 1, 1))
-    return data
-
+#%%
+"""
 aapl = pdr.get_data_yahoo('AAPL',start=datetime.datetime(2014, 10, 1), 
                           end=datetime.datetime(2020, 1, 1))
-aapl.to_csv('aapl_data.csv')
-writer = pd.ExcelWriter('aapl_data.xlsx', engine='xlsxwriter')
-aapl.to_excel(writer, sheet_name='Yahoo Data')
-
 benchmark = pdr.get_data_yahoo('SPY',start=datetime.datetime(2014, 10, 1), 
                           end=datetime.datetime(2020, 1, 1))
-
-
-#%%
-
+"""
 """ Moving Average Crossover """
-
-def strategy(data,short_window,long_window):
-    signals = pd.DataFrame(index=data.index)
-    signals['signal'] = 0.0
-    signals['shortm_avg'] = data['Close'].rolling(window=short_window,
-           min_periods=1,center=False).mean()
-    signals['longm_avg'] = data['Close'].rolling(window=long_window,
-           min_periods=1,center=False).mean()
-    signals['signal'][short_window:] = np.where(signals['shortm_avg']
-        [short_window:] > signals['longm_avg'][short_window:],1.0,0.0)
-    signals['long_short'] = signals['signal'].diff()
-    return signals
-    
+"""  
 short_window = 30
 long_window = 120
 
-
 signals = pd.DataFrame(index=aapl.index)
 signals['signal'] = 0.0
-
 signals['shortm_avg'] = aapl['Close'].rolling(window=short_window,
        min_periods=1,center=False).mean()
 signals['longm_avg'] = aapl['Close'].rolling(window=long_window,
@@ -61,13 +37,10 @@ signals['longm_avg'] = aapl['Close'].rolling(window=long_window,
 signals['signal'][short_window:] = np.where(signals['shortm_avg'][short_window:]
         > signals['longm_avg'][short_window:],1.0,0.0)
 signals['long_short'] = signals['signal'].diff()
-
 print(signals)
-signals.to_excel(writer, sheet_name='Strategy')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111,  ylabel='Price in $')
-
 aapl['Close'].plot(ax=ax1, color='r', lw=1)
 signals[['shortm_avg', 'longm_avg']].plot(ax=ax1, lw=1)
 ax1.plot(signals.loc[signals.long_short == 1.0].index, signals.shortm_avg
@@ -76,24 +49,9 @@ ax1.plot(signals.loc[signals.long_short == -1.0].index, signals.shortm_avg
          [signals.long_short == -1.0],'v', markersize=8, color='k')
 
 plt.show()
-
-#%%
-
+"""
 """ Backtesting """
-
-def portfolios(ticker, data, signals, shares, initial_capital):
-    positions = pd.DataFrame(index=signals.index)
-    positions[ticker] = shares*signals['signal']
-    portfolio = positions.multiply(data['Adj Close'],axis=0)
-    pos_diff = positions.diff() # new DataFrame
-    portfolio['holdings'] = (positions.multiply(data['Adj Close'],
-         axis=0)).sum(axis=1)
-    portfolio['cash'] = initial_capital - (pos_diff.multiply(data['Adj Close'], 
-         axis=0)).sum(axis=1).cumsum()
-    portfolio['total'] = portfolio['holdings'] + portfolio['cash']
-    portfolio['returns'] = portfolio['total'].pct_change()
-    return portfolio
-
+"""
 initial_capital = 6000
 positions = pd.DataFrame(index=signals.index)
 positions['AAPL'] = 100*signals['signal']
@@ -106,9 +64,7 @@ portfolio['cash'] = initial_capital - (pos_diff.multiply(aapl['Adj Close'],
          axis=0)).sum(axis=1).cumsum()
 portfolio['total'] = portfolio['holdings'] + portfolio['cash']
 portfolio['returns'] = portfolio['total'].pct_change()
-
 print(portfolio.head())
-portfolio.to_excel(writer, sheet_name='Portfolio')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, ylabel='Portfolio value in $')
@@ -119,41 +75,9 @@ ax1.plot(portfolio.loc[signals.long_short == -1.0].index, portfolio.total
          [signals.long_short == -1.0], 'v', markersize=8, color='k')
 
 plt.show()
-
-writer.save()
-
-#%%
-
-def backtest(data, signals, portfolio):
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot(111,  ylabel='Price in $')
-    data['Close'].plot(ax=ax1, color='r', lw=1)
-    signals[['shortm_avg', 'longm_avg']].plot(ax=ax1, lw=1)
-    ax1.plot(signals.loc[signals.long_short == 1.0].index, signals.shortm_avg
-         [signals.long_short == 1.0],'^', markersize=8, color='m')
-    ax1.plot(signals.loc[signals.long_short == -1.0].index, signals.shortm_avg
-         [signals.long_short == -1.0],'v', markersize=8, color='k')
-    plt.show()
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111, ylabel='Portfolio value in $')
-    portfolio['total'].plot(ax=ax2, lw=1)
-    ax2.plot(portfolio.loc[signals.long_short == 1.0].index, portfolio.total
-         [signals.long_short == 1.0], '^', markersize=8, color='m')
-    ax2.plot(portfolio.loc[signals.long_short == -1.0].index, portfolio.total
-         [signals.long_short == -1.0], 'v', markersize=8, color='k')
-    plt.show()
-
-def save(ticker, data, signals, portfolio):
-    file_name = ticker + '_data.xlsx'
-    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
-    data.to_excel(writer, sheet_name='Yahoo Data')
-    signals.to_excel(writer, sheet_name='Strategy')
-    portfolio.to_excel(writer, sheet_name='Portfolio')
-    writer.save()
-
-#%%
+"""
 """ Evaluations """
-
+"""
 # Sharpe Ratio : average return per unit of volatility
 returns = portfolio['returns']
 sharpe_ratio = np.sqrt(252) * (returns.mean() / returns.std())
@@ -165,7 +89,6 @@ return_data = pd.concat([bench_returns, aapl_returns], axis=1)[1:]
 return_data.columns = ['SPY', 'AAPL']
 model = sm.OLS(return_data['AAPL'],
                sm.add_constant(return_data['SPY'].values)).fit()
-
 # Alpha : gauges excess return of investment to that of market
 print('Alpha:',model.params[0])
 # Beta : measures volatility of investment to entire market
@@ -186,3 +109,100 @@ max_daily_drawdown = daily_drawdown.rolling(window, min_periods=1).min()
 daily_drawdown.plot()
 max_daily_drawdown.plot()
 plt.show()
+"""
+#%%
+
+def initialize(ticker):
+    data = pdr.get_data_yahoo(ticker,start=datetime.datetime(2014, 10, 1), 
+                          end=datetime.datetime(2020, 1, 1))
+    return data
+    
+def strategy(data,short_window,long_window):
+    signals = pd.DataFrame(index=data.index)
+    signals['signal'] = 0.0
+    signals['shortm_avg'] = data['Close'].rolling(window=short_window,
+           min_periods=1,center=False).mean()
+    signals['longm_avg'] = data['Close'].rolling(window=long_window,
+           min_periods=1,center=False).mean()
+    signals['signal'][short_window:] = np.where(signals['shortm_avg']
+        [short_window:] > signals['longm_avg'][short_window:],1.0,0.0)
+    signals['long_short'] = signals['signal'].diff()
+    return signals
+
+def portfolios(ticker, data, signals, shares, initial_capital):
+    positions = pd.DataFrame(index=signals.index)
+    positions[ticker] = shares*signals['signal']
+    portfolio = positions.multiply(data['Adj Close'],axis=0)
+    pos_diff = positions.diff() # new DataFrame
+    portfolio['holdings'] = (positions.multiply(data['Adj Close'],
+         axis=0)).sum(axis=1)
+    portfolio['cash'] = initial_capital - (pos_diff.multiply(data['Adj Close'], 
+         axis=0)).sum(axis=1).cumsum()
+    portfolio['total'] = portfolio['holdings'] + portfolio['cash']
+    portfolio['returns'] = portfolio['total'].pct_change()
+    return portfolio
+
+def backtest(data, signals, portfolio):
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111,  ylabel='Price in $')
+    data['Close'].plot(ax=ax1, color='r', lw=1)
+    signals[['shortm_avg', 'longm_avg']].plot(ax=ax1, lw=1)
+    ax1.plot(signals.loc[signals.long_short == 1.0].index, signals.shortm_avg
+         [signals.long_short == 1.0],'^', markersize=8, color='m')
+    ax1.plot(signals.loc[signals.long_short == -1.0].index, signals.shortm_avg
+         [signals.long_short == -1.0],'v', markersize=8, color='k')
+    plt.show()
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111, ylabel='Portfolio value in $')
+    portfolio['total'].plot(ax=ax2, lw=1)
+    ax2.plot(portfolio.loc[signals.long_short == 1.0].index, portfolio.total
+         [signals.long_short == 1.0], '^', markersize=8, color='m')
+    ax2.plot(portfolio.loc[signals.long_short == -1.0].index, portfolio.total
+         [signals.long_short == -1.0], 'v', markersize=8, color='k')
+    plt.show()
+    window = 252
+    rolling_max = data['Adj Close'].rolling(window, min_periods=1).max()
+    daily_drawdown = data['Adj Close']/rolling_max - 1.0
+    max_daily_drawdown = daily_drawdown.rolling(window, min_periods=1).min()
+    daily_drawdown.plot()
+    max_daily_drawdown.plot()
+    plt.show()
+
+def save(ticker, data, signals, portfolio):
+    file_name = ticker + '_data.xlsx'
+    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+    data.to_excel(writer, sheet_name='Yahoo Data')
+    signals.to_excel(writer, sheet_name='Strategy')
+    portfolio.to_excel(writer, sheet_name='Portfolio')
+    writer.save()
+
+def evaluate(ticker,data,benchmark,portfolio):
+    returns = portfolio['returns']
+    sharpe_ratio = np.sqrt(252) * (returns.mean() / returns.std())
+    print("Sharpe Ratio:",sharpe_ratio)
+    data_returns = np.log(data[['Adj Close']] / data[['Adj Close']].shift(1))
+    bench_returns = np.log(benchmark[['Adj Close']] / benchmark[['Adj Close']].shift(1))
+    return_data = pd.concat([bench_returns, data_returns], axis=1)[1:]
+    return_data.columns = ['Market', ticker]
+    model = sm.OLS(return_data[ticker],
+               sm.add_constant(return_data['Market'].values)).fit()
+    print('Alpha:',model.params[0])
+    print('Beta:',model.params[1])
+    print('R-Squared',model.rsquared)
+    days = (data.index[-1] - data.index[0]).days
+    cagr = ((((data['Adj Close'][-1]) / data['Adj Close'][1])) ** (365.0/days)) - 1
+    print("CAGR:",cagr)
+    
+
+#%%
+
+ticker = 'MSFT'
+data = initialize(ticker)
+sp500 = initialize('SPY')
+moving_avg = strategy(data,30,120)
+portfolio = portfolios(ticker, data, moving_avg, 100, 10000)
+backtest(data, moving_avg, portfolio)
+save(ticker, data, moving_avg, portfolio)
+evaluate(ticker,data,sp500,portfolio)
+
+
